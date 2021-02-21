d6fea4da22a4f30da7299e15953267f95a497cc6
turbolukoshko <turbo.lukoshko@gmail.com>
2021-02-21 19:01:59 +0200

    added mock data for boards

---
+++ src/mockBoards.js
@@ -0,0 +1,5 @@
+export const mockBoards = [
+  {id: 1, title: 'Todo', items: [{id: 1, title: 'Todo task'}, {id: 2, title: 'Todo task 2'}]},
+  {id: 2, title: 'In progress', items: [{id: 3, title: 'In progress task'}]},
+  {id: 3, title: 'Done', items: [{id: 4, title: 'Done task'}]},
+];
